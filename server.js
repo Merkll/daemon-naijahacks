aa28892e441d7e1aaeeaa6feb2d1d42f5286571e
OvieMudi <oviisaacs@gmail.com>
2019-12-09 21:27:02 +0100

    Project setup: add babel, eslint and mongoose

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-const express = require('express');
+import express from 'express';
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-
-app.get("/hello", (req, res) => {
-  res.send("Hello world");
+app.get('/hello', (req, res) => {
+  res.send('Hello world');
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
