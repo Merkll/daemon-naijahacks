6ad9df5cfb8d7a97c98ac76ca5012afc82c0ce96
OvieMudi <oviisaacs@gmail.com>
2019-12-12 08:17:12 +0100

    auth: refactor user registration

--- src/helpers/authHelper.js
+++ src/helpers/authHelper.js
@@ -1,26 +1,7 @@
-import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import { config } from 'dotenv';
 
 config();
-
-/**
-   * Hash Password Method
-   * @param {string} password
-   * @returns {string} hashed password
-   */
-const encryptString = (string) => {
-  const saltRounds = 4;
-  return bcrypt.hashSync(string, bcrypt.genSaltSync(saltRounds));
-};
-
-/**
-   * comparePassword
-   * @param {string} password
-   * @param {string} hashPassword
-   * @returns {Boolean} True or False
-   */
-const stringMatchesHash = (string, hashedString) => bcrypt.compareSync(string, hashedString);
 
 /**
    * Gnerate Token
@@ -35,4 +16,4 @@ const generateToken = (id) => jwt.sign(
   { expiresIn: '7h' },
 );
 
-export { encryptString, stringMatchesHash, generateToken };
+export default generateToken;
