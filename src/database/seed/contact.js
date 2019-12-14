89620ed4ea7f079cd1db0097d4fbfd1b93a7cde7
OvieMudi <oviisaacs@gmail.com>
2019-12-14 21:38:34 +0100

    add contacts to db seeds

--- src/database/seed/contact.js
+++ src/database/seed/contact.js
@@ -15,20 +15,30 @@ const contact2 = {
   user: uid2,
 };
 
-// const contact3 = {
-//   name: 'Ma Rythae',
-//   phoneNumber: '2348038809980',
-//   contactType: 'police-emergency',
-//   user: uid2,
-// };
-
-const contact4 = {
-  name: 'Doctor Ola',
-  phoneNumber: '2348022658731',
-  contactType: 'health-emergency',
-  user: uid1,
+const contact3 = {
+  name: 'Ma Rythae',
+  phoneNumber: '2348038809980',
+  contactType: 'police-emergency',
+  user: uid2,
 };
 
-const seedContact = () => models.Contact.insertMany([contact1, contact2, /* contact3, */ contact4]);
+
+const contact4 = {
+  name: 'Miss Odunayo',
+  phoneNumber: '2348081811957',
+  contactType: 'health-emergency',
+  user: uid2,
+};
+
+const contact5 = {
+  name: 'Engr. Miracle',
+  phoneNumber: '2348132149729',
+  contactType: 'health-emergency',
+  user: uid2,
+};
+
+const seedContact = () => models.Contact.insertMany([
+  contact1, contact2, contact3, contact4, contact5,
+]);
 
 export default seedContact;
